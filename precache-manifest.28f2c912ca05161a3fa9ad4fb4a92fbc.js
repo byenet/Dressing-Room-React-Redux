5c7e6b75a3a94f4b0c44cef22c9ce3bc06cb32d3
byenet <nhathuynguyenho@gmail.com>
2020-09-03 10:01:41 -0700

    Updates

---
+++ precache-manifest.28f2c912ca05161a3fa9ad4fb4a92fbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140d9f35aa8d218691cfb07d85991c65",
+    "url": "/Dressing-Room-React-Redux/index.html"
+  },
+  {
+    "revision": "301a57f6dfc812a4df2e",
+    "url": "/Dressing-Room-React-Redux/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "aeb86d537fc6d6fe8b1b",
+    "url": "/Dressing-Room-React-Redux/static/css/main.db8b3367.chunk.css"
+  },
+  {
+    "revision": "301a57f6dfc812a4df2e",
+    "url": "/Dressing-Room-React-Redux/static/js/2.10a28db3.chunk.js"
+  },
+  {
+    "revision": "a7b079a06d99e0bdddae2cf2b771ca7a",
+    "url": "/Dressing-Room-React-Redux/static/js/2.10a28db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb86d537fc6d6fe8b1b",
+    "url": "/Dressing-Room-React-Redux/static/js/main.9dafddbc.chunk.js"
+  },
+  {
+    "revision": "0969b53a72365f21bc58",
+    "url": "/Dressing-Room-React-Redux/static/js/runtime-main.49daa347.js"
+  }
+]);
